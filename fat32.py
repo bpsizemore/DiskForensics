e41db7df206576a6c421d3246cedf10816c76cf3
Brian Sizemore <bpsizemore@gmail.com>
2017-04-03 01:43:43 -0400

    added simple gui

--- fat32.py
+++ fat32.py
@@ -1,3 +1,9 @@
+from tkinter import *
+
+root = Tk()
+T = Text(root, height=30, width=100)
+T.pack()
+
 def print_hex(d, s):
     print(d + ' '.join(map('{:02X}'.format, s)));
 
@@ -20,25 +26,25 @@ def print_dir_entry(d, ):
         #print();
         return
 
-    print('Filename: ', d[0:8])
-    print_hex('', d)
-    print("Starting cluster: ", str(int.from_bytes(d[26:28], byteorder='little')))
-    print("Filesize in Bytes: ", str(int.from_bytes(d[28:32], byteorder='little')))
+    T.insert(END, ('Filename: ' + str(d[0:8]) + '\n'))
+    #print_hex('', d)
+    T.insert(END, ("Starting cluster: " + str(int.from_bytes(d[26:28], byteorder='little')) + '\n'))
+    T.insert(END, ("Filesize in Bytes: " + str(int.from_bytes(d[28:32], byteorder='little')) + '\n'))
 
     # iterate through fat entries
     fat_cluster = int.from_bytes(d[26:28], byteorder='little')
-    file_start = root_dir + (fat_cluster - 2)*512 - 64
+    file_start = root_dir + (fat_cluster - 2) * 512 - 64
 
     contents = data[file_start:file_start+512]
-    x = 0
-    while contents[x] != 0x00:
-        x = x + 1;
+    i = 0
+    while contents[i] != 0x00:
+        i = i + 1
 
-    print(contents[0:x])
-    print()
-    #while data[(fat_start_offset + 4 * fat_cluster + 2)] != 0xFF:
-        # swap to next fat cluster num and do stuff
-    #    print()
+    T.insert(END, 'File Contents: ' + str(contents[0:i]))
+    T.insert(END, '\n\n')
+    # while data[(fat_start_offset + 4 * fat_cluster + 2)] != 0xFF:
+    # swap to next fat cluster num and do stuff
+    # print()
 
 fat_partition = open("labsda1.img", "rb")
 
@@ -58,15 +64,15 @@ num_heads = data[26:28]
 hidden_sectors = data[28:32]
 large_sectors = data[32:36]
 disk_num = data[36]
-curr_head = data[37] #not used in FAT
+curr_head = data[37] # not used in FAT
 ex_sig = data[38]
 volume_serial = data[39:43]
 volume_label = data[43:54]
 system_id = data[54:62]
 sig = data[510:512]
-fat_start_offset = reserved_sectors * 512;
+fat_start_offset = reserved_sectors * 512
 
-#found other document saying different info
+# found other document saying different info
 
 sectors_per_fat = data[36:40]
 root_dir_first_cluster = data[44:48]
@@ -86,12 +92,14 @@ print_hex('Signature: ', sig)
 print_dec('Root Dir starts at sector: ', root_dir_first_cluster)
 
 root_dir = (520*2 + 32)*512 + 64
-print();
-print('Directory listing follows: ');
-print();
-print();
+print()
+print('Directory listing follows: ')
+print()
+print()
 
 x = 0;
 while data[root_dir+x] != 0x00:
     print_dir_entry(data[root_dir+x:root_dir+x+32])
     x = x + 32;
+
+mainloop()
